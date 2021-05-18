499d5358e28714a984e9f7f13a6d6bd1ef2fa1c3
Kaixu Xia <kaixuxia@tencent.com>
2021-05-18 12:45:05 +0800

    Revert "modules: unexport __module_address"
    This reverts commit 5af39673cb3cb4a77ce2ba375d7884553c12d68f.

--- kernel/module.c
+++ kernel/module.c
@@ -4737,6 +4737,7 @@ struct module *__module_address(unsigned long addr)
 	}
 	return mod;
 }
+EXPORT_SYMBOL_GPL(__module_address);
 
 /*
  * is_module_text_address - is this address inside module code?
